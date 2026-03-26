9cf208ef265f656565b76c265eabf42a4162e0c6
Shimar-Ahamed <shimar10106@gmail.com>
2026-03-26 18:00:01 +0530

    final latest changes

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://farmlink-heuv.onrender.com"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
     credentials: true,
